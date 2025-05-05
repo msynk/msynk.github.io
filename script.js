1360c5775a3899341074878835113359db8d0403
Saleh Yusefnejad <msynk@outlook.com>
2025-05-05 12:10:42 +0330

    add all pubKeyCredParams

--- script.js
+++ script.js
@@ -8,12 +8,17 @@ async function createCredential() {
         rp: { name: "Butil Verify" },
         user: { id: "ButilVerifyUserId", name: "ButilVerifyUser", displayName: "ButilVerifyUser" },
         authenticatorSelection: { authenticatorAttachment: "platform" },
-        pubKeyCredParams: [{ alg: -7, type: "public-key" }]
+        pubKeyCredParams: [{ alg: -7, type: "public-key" }, { alg: -8, type: "public-key" }, { alg: -257, type: "public-key" }]
     });
     console.log(result);
 }
 
 async function getCredential() {
     const result = await BitButil.webAuthn.getCredential({ challenge: "Butil Verify Challenge" });
+    // const result = await BitButil.webAuthn.getCredential({
+    //     challenge: "Butil Verify Challenge",
+    //     allowCredentials: [{ id: "ButilVerifyUserId", type: "public-key" }],
+    //     userVerification: "preferred"
+    // });
     console.log(result);
 }
