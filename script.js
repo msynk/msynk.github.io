c33137f39147d775f3ffc8b2e77081d40df6ddc2
Saleh Yusefnejad <msynk@outlook.com>
2025-05-05 11:57:32 +0330

    add butil webauthn

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function greet() {
     alert('hello world.');
 }
+
+async function createCredential() {
+    const result = await BitButil.webAuthn.createCredential({
+        challenge: "Butil Verify Challenge",
+        rp: { name: "Butil Verify" },
+        user: { id: "ButilVerifyUserId", name: "ButilVerifyUser", displayName: "ButilVerifyUser" },
+        authenticatorSelection: { authenticatorAttachment: "platform" },
+        pubKeyCredParams: [{ alg: -7, type: "public-key" }]
+    });
+    console.log(result);
+}
+
+async function getCredential() {
+    const result = await BitButil.webAuthn.getCredential({ challenge: "Butil Verify Challenge" });
+    console.log(result);
+}
